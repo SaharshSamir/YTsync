4201eadfdf977adeb82c95406f712aee4e2e0929
SaharshSamir <saharshsamir10@gmail.com>
2020-05-03 20:48:02 +0400

    blah

---
+++ app.js
@@ -0,0 +1,44 @@
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 8080;
+var socket = require('socket.io');
+
+
+var server = app.listen(port, function() {
+    console.log(`listening on port ${port}`);
+});
+
+
+
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname));
+
+var io = socket(server);
+
+var count = 0;
+
+
+io.on('connection', function (socket) {
+    
+    console.log('Connection made..');
+
+    socket.on('clientStateEvent', function(data) {
+        io.sockets.emit('serverStateEvent',data);
+    })
+
+    socket.on('clientLineEvent', function(data) {
+        io.sockets.emit('serverLineEvent', data);
+    })
+
+    socket.on('clientEvent', function(data){
+        io.sockets.emit('ServerEvent', {data})
+    })
+
+    socket.on('clientPlayerConnected', () =>  {
+        count++;
+        console.log(count);
+        io.sockets.emit('serverPlayerCount',count);
+    })
+    
+})
+
---
+++ index.js
@@ -0,0 +1,100 @@
+//Make connection
+var socket = io.connect(`http://localhost:8080`); 
+
+var people = $('#count');
+var playButton  = $('#playButton');
+var pauseButton = $('#pauseButton');
+
+
+
+//Emit events
+
+
+
+
+
+
+
+    //Emit events
+    playButton.click(function (event) {
+        console.log('play');
+        socket.emit('clientStateEvent', {
+            state:"play",
+            time:player.getCurrentTime()
+        })
+    })
+    
+    pauseButton.click(function(event) {
+        console.log('pause');
+        socket.emit('clientStateEvent', {
+            state:"pause",
+            time: player.getCurrentTime()
+        })
+    })
+
+    function playerConnected() {
+        socket.emit('clientPlayerConnected');
+    }
+
+    //Listen to events
+    socket.on('serverStateEvent', function(data) {
+        if(data.state == 'pause'){
+            player.pauseVideo();
+        }
+        if(data.state == 'play'){
+            player.playVideo();
+        }
+    })
+
+    socket.on('serverPlayerCount', count) {
+        people.innerHTML = count;s
+    }
+
+
+
+
+
+function timelineLoop()
+{
+  var line = $("#line");
+  var box = $("#box");
+  var videoLength = player.getDuration();
+
+  //Emit events  
+  line.click(function(event) {
+    //send seek request
+    var divOffset = $(this).offset();
+    var seekTo = (event.pageX - divOffset.left)/600*videoLength;
+    // console.log(divOffset);
+    socket.emit('clientLineEvent', {time: seekTo});
+    
+  });
+
+  
+
+  //Listen to events
+  socket.on('serverLineEvent', function(data) {
+    console.log(data.time);
+    player.seekTo(data.time, false);
+  })
+
+  
+
+
+  setInterval(function (){
+    if (timeline == null || player == null) {
+      return;
+    } var fraction = (player.getCurrentTime()/player.getDuration())*100;
+    box.css({left: fraction + "%"});
+    console.log(player.getCurrentTime());
+
+    socket.emit('clientEvent',{currentTime: player.getCurrentTime()});
+
+    socket.on('serverEvent', function(data){
+        player.seekTo(data.currentTime);
+      })
+    // socket.emit('clientSetState', {state: player.getPlayerState})
+  }, 200);
+
+}
+
