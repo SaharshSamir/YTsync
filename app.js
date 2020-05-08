ea5c18ce26ab218c46dc79ff9037e17d7e531f95
sparshsamir <sparshsamir1993@gmail.com>
2020-05-08 15:29:20 -0400

    nodemon added and formatting done

--- app.js
+++ app.js
@@ -1,47 +1,40 @@
-var express = require('express');
+var express = require("express");
 var app = express();
 var port = process.env.PORT || 8080;
-var socket = require('socket.io');
+var socket = require("socket.io");
 
-
-var server = app.listen(port, function() {
-    console.log(`listening on port ${port}`);
+var server = app.listen(port, function () {
+  console.log(`listening on port ${port}`);
 });
 
-
-
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 app.use(express.static(__dirname));
 
 var io = socket(server);
 
 var count = 0;
 
+io.on("connection", function (socket) {
+  console.log("Connection made.." + count);
 
-io.on('connection', function (socket) {
-    
-    console.log('Connection made..');
+  socket.on("clientStateEvent", function (data) {
+    io.sockets.emit("serverStateEvent", data);
+  });
 
-    socket.on('clientStateEvent', function(data) {
-        io.sockets.emit('serverStateEvent',data);
-    })
+  socket.on("clientLineEvent", function (data) {
+    io.sockets.emit("serverLineEvent", data);
+  });
 
-    socket.on('clientLineEvent', function(data) {
-        io.sockets.emit('serverLineEvent', data);
-    })
+  socket.on("clientEvent", function (data) {
+    io.sockets.emit("ServerEvent", { data });
+  });
 
-    socket.on('clientEvent', function(data){
-        io.sockets.emit('ServerEvent', {data})
-    })
+  socket.on("clientPlayerConnected", () => {
+    count++;
+    io.sockets.emit("serverPlayerCount", count);
+  });
 
-    socket.on('clientPlayerConnected', () =>  {
-        count++;
-        io.sockets.emit('serverPlayerCount',count);
-    })
-
-    socket.on('clientMyVideo', (data) => {
-     io.sockets.emit('serverMyVideo', data);
-    })
-    
-})
-
+  socket.on("clientMyVideo", (data) => {
+    io.sockets.emit("serverMyVideo", data);
+  });
+});
--- index.js
+++ index.js
@@ -1,133 +1,112 @@
 //Make connection
-var socket = io.connect(`http://localhost:8080`); 
-var people = $('#count');
-var playButton  = $('#playButton');
-var pauseButton = $('#pauseButton');
+var socket = io.connect(`http://localhost:8080`);
+var people = $("#count");
+var playButton = $("#playButton");
+var pauseButton = $("#pauseButton");
 var counter = 0;
-var searchBtn = $('#btn');
+var searchBtn = $("#btn");
 
 //Emit events
 playButton.click(function (event) {
-    console.log('play');
-    socket.emit('clientStateEvent', {
-        state:"play",
-        time:player.getCurrentTime()
-    })
-})
+  console.log("play");
+  socket.emit("clientStateEvent", {
+    state: "play",
+    time: player.getCurrentTime(),
+  });
+});
 
-pauseButton.click(function(event) {
-    console.log('pause');
-    socket.emit('clientStateEvent', {
-        state:"pause",
-        time: player.getCurrentTime()
-    })
-})
-
+pauseButton.click(function (event) {
+  console.log("pause");
+  socket.emit("clientStateEvent", {
+    state: "pause",
+    time: player.getCurrentTime(),
+  });
+});
 
 //Emits event that says a player has finished loading
 function playerConnected() {
-    socket.emit('clientPlayerConnected');
+  socket.emit("clientPlayerConnected");
 }
 
 //Listen to events
-socket.on('serverStateEvent', function(data) {
-    if(data.state == 'pause'){
-        player.pauseVideo();
-    }
-    if(data.state == 'play'){
-        player.playVideo();
-    }
-})
-
+socket.on("serverStateEvent", function (data) {
+  if (data.state == "pause") {
+    player.pauseVideo();
+  }
+  if (data.state == "play") {
+    player.playVideo();
+  }
+});
 
 function newPlayer() {
-    people.innerHTML = counter;
+  people.innerHTML = counter;
 }
 
 //Server gives us new count of players
-socket.on('serverPlayerCount', function(count) {
-    
-    counter = count;
-    
-})
+socket.on("serverPlayerCount", function (count) {
+  console.log("count is " + count);
+  counter = count;
+});
 
-
-
-
-
-function timelineLoop()
-{
+function timelineLoop() {
   var line = $("#line");
   var box = $("#box");
   var videoLength = player.getDuration();
 
-  //Emit events  
-  line.click(function(event) {
+  //Emit events
+  line.click(function (event) {
     //send seek request
     var divOffset = $(this).offset();
-    var seekTo = (event.pageX - divOffset.left)/600*videoLength;
+    var seekTo = ((event.pageX - divOffset.left) / 600) * videoLength;
     // console.log(divOffset);
-    socket.emit('clientLineEvent', {time: seekTo});
-    
+    socket.emit("clientLineEvent", { time: seekTo });
   });
 
-  
-
   //Listen to events
-  socket.on('serverLineEvent', function(data) {
+  socket.on("serverLineEvent", function (data) {
     console.log(data.time);
     player.seekTo(data.time);
-  })
+  });
 
-  
-
-
-  setInterval(function (){
+  setInterval(function () {
     if (timeline == null || player == null) {
       return;
-    } var fraction = (player.getCurrentTime()/player.getDuration())*100;
-    box.css({left: fraction + "%"});
-    socket.emit('clientEvent',{currentTime: player.getCurrentTime()});
+    }
+    var fraction = (player.getCurrentTime() / player.getDuration()) * 100;
+    box.css({ left: fraction + "%" });
+    socket.emit("clientEvent", { currentTime: player.getCurrentTime() });
 
-    socket.on('serverEvent', function(data){
-        player.seekTo(data.currentTime);
-      })
+    socket.on("serverEvent", function (data) {
+      player.seekTo(data.currentTime);
+    });
     // socket.emit('clientSetState', {state: player.getPlayerState})
   }, 200);
-
 }
 
 var vidId;
 
-//Set the video 
-function myVideo(){
-    searchBtn.click(function () {
-        var link;
-        var input = document.getElementById('input').value;  
-        link = new URL(input);  
-        vidId = link.search.split('=')[1];
-        socket.emit('clientMyVideo', {id: vidId});  
-    })
+//Set the video
+function myVideo() {
+  searchBtn.click(function () {
+    var link;
+    var input = document.getElementById("input").value;
+    link = new URL(input);
+    vidId = link.search.split("=")[1];
+    socket.emit("clientMyVideo", { id: vidId });
+  });
 
-    socket.on('serverMyVideo', (data) => {
-        player.cueVideoById(data.id);
-    })
+  socket.on("serverMyVideo", (data) => {
+    player.cueVideoById(data.id);
+  });
 }
-
 
 //Works
 // function myVideo(){
 //     searchBtn.click(function () {
 //         var link;
-//         var input = document.getElementById('input').value;  
-//         link = new URL(input);  
+//         var input = document.getElementById('input').value;
+//         link = new URL(input);
 //         vidId = link.search.split('=')[1];
 //         player.loadVideoById(vidId);
 //     })
 // }
-
-
-
-
-
-
